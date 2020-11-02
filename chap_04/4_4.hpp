6bd611e1e12bcfe5041515c1fad924be02d9d1ce
Soohyun Lee <sue03094@gmail.com>
2020-11-03 00:59:17 +0900

    Solve 4-4

--- chap_04/4_4.hpp
+++ chap_04/4_4.hpp
@@ -5,8 +5,27 @@
 
 #include "mytree.h"
 
-std::vector<std::list<int>> createList(BinaryTree *tree){
-  std::vector<std::list<int>> ans;
+void exploreTree(BinaryTree *tree, std::vector<std::vector<BinaryTree *>> &list, int level){
+  if(tree == nullptr) return;
+
+  std::vector<BinaryTree *> tmp;
+  
+  // list가 본 높이만큼 있는 경우, list.size() == level+1이어야 함.
+  // 따라서 list.size() == level인 경우는 아직 해당 높이가 리스트에 없는 경우임!
+  if(list.size() == level){
+    list.push_back(tmp);
+  }
+
+  list[level].push_back(tree);
+
+  exploreTree(tree->left, list, level+1);
+  exploreTree(tree->right, list, level+1);
+}
+
+std::vector<std::vector<BinaryTree *>> createList(BinaryTree *tree){
+  std::vector<std::vector<BinaryTree *>> ans;
+
+  exploreTree(tree, ans, 0);
 
   return ans; 
 }
